150e824f0b45dec79d273ed809d01427de855480
raquelgl20 <161474670+raquelgl20@users.noreply.github.com>
2024-05-08 09:09:57 -0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,10 @@
+
+let numero = prompt ('informe um numero: ');
+
+if (numero>=0){
+    alert('o número digitado é positivo!! ');
+} else  if(numero<0){
+    alert('o número digitado é negativo!! ');
+} else {
+    alert('valor inválido ');
+}
